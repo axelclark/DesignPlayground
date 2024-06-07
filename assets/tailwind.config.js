4a99d27b4ca76bf2077e541e789b8f4d829a9ee5
Axel Clark <axel.clark@pm.me>
2024-06-07 14:35:07 -0700

    Add examples

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,21 +1,45 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-const plugin = require("tailwindcss/plugin")
-const fs = require("fs")
-const path = require("path")
+const plugin = require("tailwindcss/plugin");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   content: [
     "./js/**/*.js",
     "../lib/design_playground_web.ex",
-    "../lib/design_playground_web/**/*.*ex"
+    "../lib/design_playground_web/**/*.*ex",
   ],
   theme: {
     extend: {
+      animation: {
+        "slide-in": "slide-in 1s ease-in-out",
+        "zoom-in": "zoom-in 1s",
+        "fade-in": "fade-in 1s both",
+        "fade-out": "fade-out 500ms both",
+      },
+      keyframes: {
+        "slide-in": {
+          from: { transform: "translateX(-100%)" },
+          to: { transform: "translateX(0%)" },
+        },
+        "zoom-in": {
+          from: { opacity: 0, transform: "scale3d(0.3, 0.3, 0.3)" },
+          "50%": { opacity: 1 },
+        },
+        "fade-in": {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        "fade-out": {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
+      },
       colors: {
         brand: "#FD4F00",
-      }
+      },
     },
   },
   plugins: [
@@ -25,51 +49,77 @@ module.exports = {
     //
     //     <div class="phx-click-loading:animate-ping">
     //
-    plugin(({addVariant}) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
-    plugin(({addVariant}) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"])),
+    plugin(({ addVariant }) =>
+      addVariant("phx-no-feedback", [
+        ".phx-no-feedback&",
+        ".phx-no-feedback &",
+      ]),
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-click-loading", [
+        ".phx-click-loading&",
+        ".phx-click-loading &",
+      ]),
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-submit-loading", [
+        ".phx-submit-loading&",
+        ".phx-submit-loading &",
+      ]),
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-change-loading", [
+        ".phx-change-loading&",
+        ".phx-change-loading &",
+      ]),
+    ),
 
     // Embeds Heroicons (https://heroicons.com) into your app.css bundle
     // See your `CoreComponents.icon/1` for more information.
     //
-    plugin(function({matchComponents, theme}) {
-      let iconsDir = path.join(__dirname, "../deps/heroicons/optimized")
-      let values = {}
+    plugin(function ({ matchComponents, theme }) {
+      let iconsDir = path.join(__dirname, "../deps/heroicons/optimized");
+      let values = {};
       let icons = [
         ["", "/24/outline"],
         ["-solid", "/24/solid"],
         ["-mini", "/20/solid"],
-        ["-micro", "/16/solid"]
-      ]
+        ["-micro", "/16/solid"],
+      ];
       icons.forEach(([suffix, dir]) => {
-        fs.readdirSync(path.join(iconsDir, dir)).forEach(file => {
-          let name = path.basename(file, ".svg") + suffix
-          values[name] = {name, fullPath: path.join(iconsDir, dir, file)}
-        })
-      })
-      matchComponents({
-        "hero": ({name, fullPath}) => {
-          let content = fs.readFileSync(fullPath).toString().replace(/\r?\n|\r/g, "")
-          let size = theme("spacing.6")
-          if (name.endsWith("-mini")) {
-            size = theme("spacing.5")
-          } else if (name.endsWith("-micro")) {
-            size = theme("spacing.4")
-          }
-          return {
-            [`--hero-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
-            "-webkit-mask": `var(--hero-${name})`,
-            "mask": `var(--hero-${name})`,
-            "mask-repeat": "no-repeat",
-            "background-color": "currentColor",
-            "vertical-align": "middle",
-            "display": "inline-block",
-            "width": size,
-            "height": size
-          }
-        }
-      }, {values})
-    })
-  ]
-}
+        fs.readdirSync(path.join(iconsDir, dir)).forEach((file) => {
+          let name = path.basename(file, ".svg") + suffix;
+          values[name] = { name, fullPath: path.join(iconsDir, dir, file) };
+        });
+      });
+      matchComponents(
+        {
+          hero: ({ name, fullPath }) => {
+            let content = fs
+              .readFileSync(fullPath)
+              .toString()
+              .replace(/\r?\n|\r/g, "");
+            let size = theme("spacing.6");
+            if (name.endsWith("-mini")) {
+              size = theme("spacing.5");
+            } else if (name.endsWith("-micro")) {
+              size = theme("spacing.4");
+            }
+            return {
+              [`--hero-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
+              "-webkit-mask": `var(--hero-${name})`,
+              mask: `var(--hero-${name})`,
+              "mask-repeat": "no-repeat",
+              "background-color": "currentColor",
+              "vertical-align": "middle",
+              display: "inline-block",
+              width: size,
+              height: size,
+            };
+          },
+        },
+        { values },
+      );
+    }),
+  ],
+};
--- lib/design_playground_web/live/home_live.ex
+++ lib/design_playground_web/live/home_live.ex
@@ -4,9 +4,35 @@ defmodule DesignPlaygroundWeb.HomeLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <div>
-      Hello World!
+    <.button class="transition ease-in-out delay-150 !bg-blue-500 hover:-translate-y-1 hover:scale-105 hover:!bg-indigo-500 duration-300">
+      Save Changes
+    </.button>
+    <div class="mt-12">
+      <.button class="animate-fade-out">
+        Save Changes
+      </.button>
     </div>
+    <div class="mt-12">
+      <.button phx-click="animate">
+        Animate
+      </.button>
+    </div>
+    <div class="mt-12">
+      <.button id="animate-button" data-animate={JS.add_class("animate-fade-out")}>
+        Animated
+      </.button>
+    </div>
+    <div id="item">My Item</div>
+    <button phx-click={
+      JS.transition({"ease-out duration-300 both", "opacity-100", "opacity-0"}, to: "#item")
+    }>
+      Shake!
+    </button>
     """
+  end
+
+  @impl true
+  def handle_event("animate", _params, socket) do
+    {:noreply, push_event(socket, "animate", %{id: "animate-button"})}
   end
 end
