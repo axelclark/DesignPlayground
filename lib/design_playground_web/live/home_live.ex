95ae787fdfc36eede1a7e700801d95e36c39f53a
Axel Clark <axel.clark@pm.me>
2024-06-17 11:31:41 -0700

    Add headings

--- lib/design_playground_web/live/home_live.ex
+++ lib/design_playground_web/live/home_live.ex
@@ -4,15 +4,18 @@ defmodule DesignPlaygroundWeb.HomeLive do
   @impl true
   def render(assigns) do
     ~H"""
+    <h2>Animate with hover modifier</h2>
     <.button class="transition ease-in-out delay-150 !bg-blue-500 hover:-translate-y-1 hover:scale-105 hover:!bg-indigo-500 duration-300">
       Save Changes
     </.button>
     <div class="mt-12">
+    <h2>Animation incorporated as tailwindcss class (see how to add with push event next)</h2>
       <.button class="animate-fade-out">
         Save Changes
       </.button>
     </div>
     <div class="mt-12">
+    <h2>Animation via Hook (send event)</h2>
       <.button phx-click="animate">
         Animate
       </.button>
@@ -22,12 +25,15 @@ defmodule DesignPlaygroundWeb.HomeLive do
         Animated
       </.button>
     </div>
+    <div class="mt-12">
+    <h2>Animation via JS library</h2>
     <div id="item">My Item</div>
-    <button phx-click={
+    <.button phx-click={
       JS.transition({"ease-out duration-300 both", "opacity-100", "opacity-0"}, to: "#item")
     }>
-      Shake!
-    </button>
+      Animate
+    </.button>
+    </div>
     """
   end
 
